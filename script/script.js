281a891002c2e87035323475f1c33aae9ed400ba
Mikhail <etozhenerk@yandex.ru>
2021-10-29 19:15:58 +0300

    form dataBase

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
 "use strict";
 
+const dataBase = [];
+
 const modalAdd = document.querySelector(".modal__add");
 const addAd = document.querySelector(".add__ad");
 const modalBtnSubmit = document.querySelector(".modal__btn-submit");
 const modalSubmit = document.querySelector(".modal__submit");
 const modalItem = document.querySelector(".modal__item");
 const catalog = document.querySelector(".catalog");
+const modalBtnWarning = document.querySelector(".modal__btn-warning");
+
+const elementsModalSubmit = [...modalSubmit.elements].filter(
+    (item) => item.tagName !== "BUTTON" && item.type !== "submit",
+);
 
 const closeModal = function (e) {
     const target = e.target;
@@ -28,4 +35,20 @@ catalog.addEventListener("click", (e) => {
     if (target.closest(".card")) {
         modalItem.classList.remove("hide");
     }
+});
+
+modalSubmit.addEventListener("input", () => {
+    const validForm = elementsModalSubmit.every((item) => item.value);
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? "none" : "";
+});
+modalSubmit.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const itemObj = {};
+
+    for(const item of elementsModalSubmit){
+        itemObj[item.name] = item.value;
+    }
+    dataBase.push(itemObj);
+    modalSubmit.reset();
 });
